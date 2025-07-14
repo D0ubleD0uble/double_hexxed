5a0fcf7487c7e01063189821d42f573e049b74d5
Cole Hinkel <colerhinkel@gmail.com>
2025-07-13 23:11:03 -0500

    only one build required

--- build.sh
+++ build.sh
@@ -4,11 +4,8 @@ set -e
 # Clean previous output
 rm -rf dist/
 
-# Build the project to WebAssembly
-cargo build --target wasm32-unknown-unknown --release
-
 # Build with wasm-pack (outputs to ./pkg by default)
-wasm-pack build --release --target web --out-dir dist/pkg
+wasm-pack build --release --target web --out-dir dist/
 
 # Copy static files to dist
 cp index.html dist/
